91e2f96b42dcfe36683b3ad327232400104f9fc4
Ayoub Guidi <ayoubgd19@gmail.com>
2026-09-25 11:35:22 +0000

    fix: optimize image transformations

--- next.config.ts
+++ next.config.ts
@@ -19,10 +19,28 @@ const nextConfig: NextConfig = {
 
   // Optimize images
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 2678400,
+    formats: ["image/webp","image/avif"],
+    deviceSizes: [
+      640,
+      750,
+      828,
+      1080,
+      1200,
+      1920,
+    ],
+
+    imageSizes: [
+      16,
+      32,
+      48,
+      64,
+      96,
+      128,
+      256,
+      384,
+    ],
+    qualities: [75],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
